2741d1d115bf88ab1d8069172b14632d49f5099e
YFRP01 <rahilpavel533@gmail.com>
2026-02-16 13:31:54 +0100

    latest update

--- frontend/components/PreviewImage.jsx
+++ frontend/components/PreviewImage.jsx
@@ -6,10 +6,12 @@ const PreviewImage = ({image, setIsOpen}) => {
   return (
     <div onClick={()=>(setIsOpen(false))} className={`fixed z-100 w-screen h-screen inset-0 bg-black/90 `}>
         <div className='w-full h-full flex flex-col items-center'>
-            {/* <div className='flex w-full bg-black shadow-lg p-2'>
-                <ArrowLeftToLineIcon onClick={()=>(setIsOpen(false))} size={30} className='text-white cursor-pointer'/>
-            </div> */}
-            <div className='h-full w-full backdrop-blur-xs flex items-center justify-center'><img src={image} className={`bg-white md:w-[80%] md:h-[80%]`}/> </div>   
+            <div className='h-full w-full backdrop-blur-xs flex items-center justify-center'>
+                {image ? (
+                  <img src={image} className={`bg-white md:w-[80%] md:h-[80%]`}/>
+                ):
+                (<div className='text-white text-lg lg:text-lg'>No Image Found!</div>)}
+             </div>   
         </div> 
     </div>
   )
--- frontend/pages/developerPages/EditItem.jsx
+++ frontend/pages/developerPages/EditItem.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useRef, useState } from 'react'
 import { API_URL } from '../../api'
 import { sources } from '../../src/assets/assets'
-import { ChevronDown, ChevronUp, Edit, Edit2, Plus, PlusCircle, X } from 'lucide-react'
+import { ChevronDown, ChevronUp, Edit, Edit2, Plus, PlusCircle, Pointer, Trash, X } from 'lucide-react'
 import PreviewImage from '../../components/PreviewImage'
 import { useParams } from 'react-router-dom'
 
@@ -11,50 +11,52 @@ const EditItem = () => {
   const {id} = useParams()
   const ref = useRef()
 
-  const beginIndex = 3
+  const beginIndex = 1
   const [categories, setCategories] = useState([])
-  const [nameInput, setNameInput] = useState('')
   const [descriptionInput, setDescriptionInput] = useState('')
   const [categoryInput, setCategoryInput] = useState('')
-  const [sourceInput, setSourceInput] = useState('')
   const [isViewCategory, setViewCategory] = useState(false)
   const [isViewSource, setViewSource] = useState(false)
   const [isPreviewCard, setIsPreviewCard] = useState(false)
   const [displayNum, setDisplayNum] = useState(beginIndex)
   const [viewCat, setViewCat] = useState(false)
-  const [catImages, setCatImages] = useState()
   const [selectedImageIndex, setSelectedImageIndex] = useState(null)
-  const [holdInfos, setHoldInfos] = useState([])
-  const [images, setImages] = useState([])
-  const [infoBackup, setInfoBackup] = useState([])
+  const [backUpData, setBackupData] = useState(null)
+
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    category: '',
+    source: '',
+    infos: [],
+    thumbnails: [],
+    categoryImage: {}
+  })
 
   const getDetails = async()=>{
     try {
         const response = await axios.get(`${API_URL}/items/developer/${id}`)
         const temp = response.data[0]
-        setHoldInfos(temp.infos)
-        setInfoBackup(temp.infos)
-        setImages(temp.thumbnails)
-        setNameInput(temp.name)
-        setDescriptionInput(temp.description)
-        setSourceInput(temp.source)
-        setCategoryInput(temp.category)
-        setCatImages(temp.category_image)
-        
+        const newData = {
+          name: temp.name,
+          description: temp.description,
+          category: temp.category,
+          source: temp.source,
+          infos: temp.infos,
+          thumbnails: temp.thumbnails,
+          category_image: temp.category_image
+        }
+        setFormData(newData)
+        setBackupData(newData)
     } catch (error) {
         console.log(`${error.message}`);
     }
   }
 
-  
-  const clearForm = ()=>{
-      setNameInput('')
-      setDescriptionInput('')
-      setCategoryInput('')
-      setSourceInput('')
-      setHoldInfos([{qty: "", cost: "", details: ""}])
-      setImages([])
-      setCatImages({})
+  const resetForm = ()=>{
+      if(backUpData){
+        setFormData(backUpData)
+      }
   }
   const handleShow = (e, value, input)=>{
         if(e.key === 'Enter' && input.trim()){
@@ -63,14 +65,14 @@ const EditItem = () => {
   }
 
   const handleChange = (e, field, index)=>{
-    const update = [...holdInfos];
+    const update = [...formData.infos];
     update[index][field] = e;
-    setHoldInfos(update)
+    setFormData((prev)=> ({...prev, infos: update}))
   }
 
   const addNewBlock = ()=>{
-    setHoldInfos([...holdInfos, {qty: "", cost: "", details: ""}]); 
-    setDisplayNum(holdInfos.length + 1)
+    setFormData((prev)=>({...prev, infos: [...prev.infos, {qty: "", cost: "", details: ""}]})); 
+    setDisplayNum(formData.infos.length + 1)
   }
 
   const handleInsertImage = (e)=>{
@@ -80,7 +82,7 @@ const EditItem = () => {
     for(let i=0; i < files.length; i++){
         update.push({file: files[i], image_url: URL.createObjectURL(files[i])})
     } 
-    setImages((prev)=>[...prev, ...update])
+    setFormData((prev)=>({...prev, thumbnails: [...prev.thumbnails, ...update]}))
     e.target.value=""
   }
 
@@ -88,43 +90,48 @@ const EditItem = () => {
     const file = e.target.files[0]
     const update = {file, image_url: URL.createObjectURL(file)}
     if(!file) return;
-    setCatImages(update)
+    setFormData((prev=>({...prev, categoryImage: update})))
     e.target.value=""
-    
-
   }
+
   const handleDeleteImage = (index) =>{
-    setImages((prev)=>prev.filter((_, i)=> i !== index))
+        setFormData((prev)=>({...prev, thumbnails: prev.thumbnails.filter((_, i)=> i !== index)}))
+    // setImages((prev)=>prev.filter((_, i)=> i !== index))
   }
 
   const handleEditImage = (e, index)=>{
     const file= e.target.files[0]
     if(!file) return;
     const update = {image_url: URL.createObjectURL(file), file}
-    setImages((prev)=>prev.map((img, i)=>( i=== index ? update : img)))
+    setFormData((prev)=>({...prev, thumbnails: prev.thumbnails.map((img, i)=>( i === index ? update : img))}))
   }
 
   const handleRadio = (e, input, type)=>{
     if(type.includes('cat')){
         setTimeout(() => {
-          setCategoryInput(input)
+          setFormData(prev=>({...prev, category: input}))
           setViewCategory(false)
         }, 300);
     }
     else if(type.includes('source')) {
         setTimeout(() => {
-          setSourceInput(input)
+          setFormData(prev=>({...prev, source: input}))
           setViewSource(false)
         }, 300);
     }
     else {
-      console.log('error');
+      console.log('Error in handleRadio: type not recognized -', type);
       return null
     }
   }
 
+  const handleDelInfo = (index) =>{
+    const update = formData.infos.filter((_, i)=> i !== index)
+    setFormData(prev=>({...prev, infos: update}))
+  }
+
   const handleInfoMore = () =>{
-        if(displayNum === beginIndex) setDisplayNum(holdInfos.length); 
+        if(displayNum === beginIndex) setDisplayNum(formData.infos.length); 
         else setDisplayNum(beginIndex);      
   }
 
@@ -143,7 +150,7 @@ const EditItem = () => {
   
   useEffect(()=>{
     allCategories()
-    holdInfos
+    formData.infos
   }, [])
 
   useEffect(()=>{
@@ -159,6 +166,9 @@ const EditItem = () => {
         document.removeEventListener('mousedown', handle)
     })
     }, [])
+
+    console.log(formData.category);
+    
     
 
   return (
@@ -172,8 +182,8 @@ const EditItem = () => {
             <div className='w-full flex justify-between items-center'>
               <p className='text-blue-500'>Item</p>
               <div className='w-full font-medium text-sm text-white flex justify-end items-center gap-3 px-2 py-1'>
-                <button onClick={()=>(setHoldInfos([]), setHoldInfos(infoBackup))} className='bg-orange-400 border-none w-20 h-7 md:w-25 md:h-8 rounded-md cursor-pointer'>Reset</button>
-                <button onClick={()=>(clearForm())} className='bg-green-500 border-none w-20 h-7 md:w-25 md:h-8 p-1 rounded-md cursor-pointer'>Submit</button>
+                <button onClick={()=>(resetForm())} className='bg-orange-400 border-none w-20 h-7 md:w-25 md:h-8 rounded-md cursor-pointer'>Reset</button>
+                <button onClick={()=>(resetForm())} className='bg-green-500 border-none w-20 h-7 md:w-25 md:h-8 p-1 rounded-md cursor-pointer'>Submit</button>
               </div>
 
             </div>
@@ -181,7 +191,7 @@ const EditItem = () => {
           <div className='flex gap-2 p-1'>
             <h2 className='font-medium'>Name <span className='text-red-500'>*</span></h2>
             <input 
-            type='text' value={nameInput} placeholder='Enter the item name ...' onChange={((e)=>(setNameInput(e.target.value)))} 
+            type='text' value={formData?.name} placeholder='Enter the item name ...' onChange={((e)=>(setFormData((prev)=>({...prev, name: e.target.value}))))} 
             className={`focus:ring-2 flex-1 border border-gray-300 rounded-sm outline-none ring-blue-500 bg-gray-100 px-2 text-gray-800` }
             required/>
           </div>
@@ -189,7 +199,7 @@ const EditItem = () => {
           <div className='flex gap-2 p-1'>
             <h2 className='font-medium'>Description</h2>
             <textarea 
-            onKeyDown={(e)=>(handleShow(e, 'Description', descriptionInput))} value={descriptionInput} placeholder='Enter the item name ...' onChange={((e)=>(setDescriptionInput(e.target.value)))} 
+            onKeyDown={(e)=>(handleShow(e, 'Description', descriptionInput))} value={formData?.description} placeholder='Enter the item name ...' onChange={((e)=>(setFormData((prev)=>({...prev, description: e.target.value}))))} 
             className={`focus:ring-2 flex-1 border border-gray-300 rounded-sm outline-none ring-blue-500 bg-gray-100 px-2 text-gray-800` }
             />
           </div>
@@ -211,7 +221,7 @@ const EditItem = () => {
               {!viewCat && (                  
                   <div className='lg:flex-1 flex items-center justify-center gap-2'>
                     <label className='w-full flex items-center '>
-                    <textarea placeholder='select' readOnly value={categoryInput} onClick={()=>(setViewCategory(!isViewCategory))} 
+                    <input type='text' placeholder='select' readOnly value={formData?.category} onClick={()=>(setViewCategory(!isViewCategory), setFormData((prev)=>({...prev, category: ''})))} 
                     className='w-full h-full border border-gray-500 cursor-pointer text-gray-500 px-1 rounded-sm'/>
                     </label>
                   </div>
@@ -221,10 +231,14 @@ const EditItem = () => {
                         <label className='w-full flex items-center'>
                           <textarea type='text' 
                           className={`focus:ring-2 h-full w-full border border-gray-300 rounded-sm outline-none ring-blue-500 bg-gray-100 px-2 text-gray-800` } 
-                          placeholder='Custom name ...' value={categoryInput} onChange={(e)=>(setCategoryInput(e.target.value))}/>
+                          placeholder='Custom name ...' value={formData?.category} onChange={(e)=>(setFormData((prev)=>({...prev, category: e.target.value})))}/>
                         </label>
                         <label className='flex items-center justify-center w-full'>
-                            <img src={catImages?.image_url} alt={catImages ? 'category preview':'Select image'} className='object-cover h-20 lg:h-30 w-full rounded-md border text-gray-500 text-sm border-gray-300'/>
+                          <div className='h-20 lg:h-30 w-full bg-white rounded-md border text-gray-500 text-sm border-gray-300'>
+                            {formData?.categoryImage?.image_url ? (<img src={formData?.categoryImage?.image_url} alt='category preview' className='object-cover h-full w-full'/>):
+                            (<p className='text-center h-full flex items-center justify-center gap-2'><Pointer size='20' /><span>Select image</span></p>)}
+                          </div>
+                            {/* <img src={formData?.categoryImage?.image_url} alt={formData?.categoryImage ? 'category preview':'Select image'} className='object-cover h-20 lg:h-30 w-full rounded-md border text-gray-500 text-sm border-gray-300'/> */}
                             <input type='file' accept='image/*' onChange={(e)=>(handleInsertCategoryImage(e))} className='hidden'/>
                         </label>
                   </div>
@@ -233,11 +247,11 @@ const EditItem = () => {
             {isViewCategory && (
                 <div className='fixed top-0 left-0 right-0 bottom-0 bg-black/5 transition-all duration-500 ease-in-out w-full h-screen z-53'>
                   <div className='bg-ed-500 h-full w-full flex justify-center relative'>
-                   <div ref={ref} className='absolute top-63 w-64 transition-all duration-500 ease-in-out max-h-100 overflow-y-auto bg-white rounded-lg border border-blue-300 text-gray-800 p-2 flex flex-col gap-1'>
+                   <div ref={ref} className='absolute top-43 w-64 transition-all duration-500 ease-in-out max-h-100 overflow-y-auto bg-white rounded-lg border border-blue-300 text-gray-800 p-2 flex flex-col gap-1'>
                         {categories.map((cat, index)=>(
                         <label key={cat.category_id} onClick={(e)=>(handleRadio(e, cat.category_name, 'cat'))} className={`flex gap-1 hover:bg-blue-100 px-1 rounded-md break-all`}>
                             <input
-                            type='radio' onChange={(e)=>(setCategoryInput(e.target.value))} value={cat.category_name} name='category' required={index === 0}/>
+                            type='radio' value={cat.category_name} name='category' required={index === 0}/>
                             {cat.category_name}
                         </label>
                       ))}
@@ -251,7 +265,7 @@ const EditItem = () => {
             <div className='flex gap-2'>
               <p className='font-medium'>Source</p>
               <button onClick={()=>(setViewSource(!isViewSource))} className='border border-gray-500 min-w-30 cursor-pointer text-gray-500 px-1 rounded-sm'>
-                {sourceInput? sourceInput:'select'}
+                {formData.source? formData.source : 'select'}
               </button>
             </div>
             {isViewSource && (
@@ -261,7 +275,7 @@ const EditItem = () => {
                         {sources?.map((s)=>(
                         <label key={s.id} onClick={(e)=>(handleRadio(e, s.name, 'source'))} className={`flex gap-1 hover:bg-blue-100 px-1 rounded-md break-all`}>
                             <input
-                            type='radio' onChange={(e)=>(setSourceInput(e.target.value))} value={s.name} name='source' />
+                            type='radio' onChange={(e)=>(setFormData((prev)=>({...prev, source:e.target.value})))} value={s.name} name='source' />
                             {s.name}
                         </label>
                       ))}
@@ -279,9 +293,14 @@ const EditItem = () => {
       -----------------------------------------*/}
       <div>
       <div className='flex flex-col'>
-          <p className='text-blue-500'>Infos ({holdInfos.length})</p>
-          {holdInfos?.slice(0,displayNum).map((i, index)=>(
-            <div key={index} className={`border-t border-blue-200 transition-all duration-700 ease-in-out`}>
+          <p className='text-blue-500'>Infos ({formData?.infos.length})</p>
+          {formData?.infos.slice(0,displayNum).map((i, index)=>(
+            <div key={index} className={`border-t relative border-blue-200 transition-all duration-700 ease-in-out`}>
+              <div className='flex justify-between px-2 p-1 text-sm'>
+                <p className='bg-blue-400 border border-gray-600 text-white p-1 w-7 h-7 flex items-center justify-center rounded-full'>{index+1}</p>
+                <Trash onClick={()=>(handleDelInfo(index))} className='text-red-500 w-7 h-7 cursor-pointer'/>
+              </div>
+         
           {/* Qty */}
           <div className='flex gap-2 p-1'>
             <h2 className='flex gap-1 font-medium'>Quantity <span className='text-red-500'>*</span></h2>
@@ -314,13 +333,15 @@ const EditItem = () => {
                 <PlusCircle className='w-5 h-5' />
                 Add More Infos
             </button>
-          {holdInfos.length>0 && <div className='flex items-center justify-center text-blue-500'>
+          {formData.infos.length>0 && <div className='flex items-center justify-center text-blue-500'>
+          {formData.infos.length > beginIndex &&
             <button onClick={()=>(handleInfoMore())} className='flex gap-1 items-center justify-center p-2 bg-blue-500 text-white rounded-lg outline'>
                 {displayNum !== beginIndex ? 
                 (<span className='flex gap-1 items-center justify-center'>Less <ChevronUp /></span>)
                 : 
                 (<span className='flex gap-1 items-center justify-center'>More <ChevronDown /></span>)}
             </button>
+          }
         </div>}
         </div>
       </div>
@@ -331,10 +352,10 @@ const EditItem = () => {
       -----------------------------------------*/}
 
         <div className='w-full'>
-          <p className='text-blue-500'>Thumbnails ({images.length}) <span className='text-red-500'>*</span></p>
+          <p className='text-blue-500'>Thumbnails ({formData?.thumbnails.length}) <span className='text-red-500'>*</span></p>
           <div className='flex border border-gray-200 p-1 gap-3 w-full m-1 h-60 overflow-x-auto'>
             <div className='flex gap-2 p-1'>
-                {images?.map((i, index)=>(
+                {formData.thumbnails?.map((i, index)=>(
                 <div key={index} className='relative flex flex-col items-center justify-center w-50 rounded-md text-gray-600 border border-gray-400 bg-white'>
                     <img onClick={()=>(setSelectedImageIndex(index), setIsPreviewCard(true))} src={i?.image_url} alt='preview' className='w-full h-full object-cover border border-gray-200'/>
                     <div className='absolute top-0 left-0 flex justify-end gap-2 p-2 items-center right-0 w-full h-5'>
@@ -356,7 +377,7 @@ const EditItem = () => {
             </div>
           </div>
         </div>
-          {isPreviewCard && images[selectedImageIndex] && <PreviewImage image={images[selectedImageIndex].image_url} setIsOpen={setIsPreviewCard}/>}  
+          {isPreviewCard && formData.thumbnails[selectedImageIndex] && <PreviewImage image={formData.thumbnails[selectedImageIndex].image_url} setIsOpen={setIsPreviewCard}/>}  
     </div>
   )
 }
@@ -402,7 +423,7 @@ export default EditItem
 //   const [viewCat, setViewCat] = useState(false)
 //   const [catImages, setCatImages] = useState()
 //   const [selectedImageIndex, setSelectedImageIndex] = useState(null)
-//   const [holdInfos, setHoldInfos] = useState([])
+//   const [formData.infos, setHoldInfos] = useState([])
 //   const [images, setImages] = useState([])
 //   const [infoBackup, setInfoBackup] = useState([])
 
@@ -457,7 +478,7 @@ export default EditItem
 //     }
 //   }
   
-//   const clearForm = () => {
+//   const resetForm = () => {
 //     setNameInput('')
 //     setDescriptionInput('')
 //     setCategoryInput('')
@@ -594,7 +615,7 @@ export default EditItem
 //             Reset Changes
 //           </button>
 //           <button 
-//             onClick={clearForm}
+//             onClick={resetForm}
 //             className='flex items-center gap-2 px-5 py-2.5 bg-emerald-600 hover:bg-emerald-700 text-white font-medium rounded-lg transition-all duration-200 shadow-md hover:shadow-lg transform hover:-translate-y-0.5'
 //           >
 //             <Save size={18} />
