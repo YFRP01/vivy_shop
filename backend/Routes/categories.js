7c7d2801b5a3f7ed2646e68210b3763e42ba4032
YFRP01 <rahilpavel533@gmail.com>
2026-02-16 20:46:02 +0100

    latest update

--- backend/Routes/categories.js
+++ backend/Routes/categories.js
@@ -15,5 +15,42 @@ router.get("/", async (req, res)=>{
     }
 })
 
+router.get("/developer", async (req, res)=>{
+    try {
+        const {category} = req.query
+        const cat = []
+        let holdQuery = `
+            SELECT cat.category_id, cat.category_name,
+                COALESCE (
+                    json_agg(
+                        json_build_object(
+                            'item_id', i.item_id,
+                            'name', i.name,
+                            'category', cat.category_name,
+                            'date', i.created_at::date,
+                            'time', i.created_at::time
+                        ) ORDER BY i.name ASC
+                ) FILTER (WHERE i.item_id IS NOT NULL), 
+                '[]' 
+                ) AS item
+                FROM categories cat 
+                JOIN items i ON cat.category_id = i.category_id
+                `    
+    if(category && category !== 'all'){
+        cat.push(`%${category}%`)
+        holdQuery += ` WHERE cat.category_name ILIKE $1 `
+    }
+    
+    holdQuery+= ` GROUP BY cat.category_id ORDER BY cat.created_at DESC  `
+    
+    const response = await pool.query(holdQuery, cat)
+    res.status(200).json(response.rows)
+
+    } catch (error) {
+        res.status(500).json(`Unable to fetch categories: ${error.message}`)
+    }
+})
+
+
 
 export default router
--- frontend/pages/developerPages/ListCats.jsx
+++ frontend/pages/developerPages/ListCats.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { API_URL } from '../../api'
+import axios from 'axios'
 
 const ListCats = () => {
+
+    const [searchParams] = useSearchParams()
+    const [items, setItems] = useState([])
+    const category = searchParams.get('category') || 'all';
+    const getAll = async()=>{
+        try {
+            const response = await axios.get(`${API_URL}/categories/developer?category=${category}`)
+            setItems(response.data)
+        } catch (error) {
+            console.log(`Unable to get the saved items ${error.message}`);
+        }
+    }
+
+    useEffect(()=>{
+        getAll()
+    }, [])
+
   return (
     <div>
-      
+      {JSON.stringify(items)}
     </div>
   )
 }
